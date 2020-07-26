6c8cbf3ee7c7fe11edc90f7ae79aecc2a5118aae
carstefano <carstefano@hotmail.com>
2020-07-26 18:35:29 -0500

    first commit

---
+++ precache-manifest.3e767d67ef470c1a9047e201a93dd5cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f853bb5b0cd5322a47d25b4db8e5f7b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90aade3aa79eee3834ce",
+    "url": "./static/css/main.98f4779b.chunk.css"
+  },
+  {
+    "revision": "13431063e241afe8af85",
+    "url": "./static/js/2.a0dcc4ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a0dcc4ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90aade3aa79eee3834ce",
+    "url": "./static/js/main.1b6e1a44.chunk.js"
+  },
+  {
+    "revision": "167efae3035c3ef86d01",
+    "url": "./static/js/runtime-main.743fcd3a.js"
+  }
+]);
